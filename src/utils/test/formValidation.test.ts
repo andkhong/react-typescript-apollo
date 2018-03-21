b4ca6bb23b4b0484acd7a7cf3a4c490773fd82c7
An Khong <andkhong>
2018-03-20 17:40:09 -0700

    added better naming

--- src/utils/test/formValidation.test.ts
+++ src/utils/test/formValidation.test.ts
@@ -5,22 +5,26 @@ import {
   isLastNameValid
 } from 'utils/formValidation';
 
-test('Email Validator should be working', () => {
-  expect(isEmailValid('andkhong@gmail.com')).toBe(true);
-  expect(isEmailValid('andkhong@.com')).toBe(false);
-});
+describe('formValidation.ts', () => {
 
-test('Password Validator should be working', () => {
-  expect(isPasswordValid('HelloWorld01!')).toBe(true);
-  expect(isPasswordValid('ApplesBannan')).toBe(false);
-  expect(isPasswordValid('        ')).toBe(false);
-  expect(isPasswordValid('littleLittle10')).toBe(false);
-});
-
-test('First Name Validator should be working', () => {
-  expect(isFirstNameValid('andy ')).toBe(true);
-});
-
-test('Last Name Validator should be working', () => {
-  expect(isLastNameValid('khong')).toBe(true);
-});
+  it('Email Validator should be working', () => {
+    expect(isEmailValid('andkhong@gmail.com')).toBe(true);
+    expect(isEmailValid('andkhong@.com')).toBe(false);
+  });
+  
+  it('Password Validator should be working', () => {
+    expect(isPasswordValid('HelloWorld01!')).toBe(true);
+    expect(isPasswordValid('ApplesBannan')).toBe(false);
+    expect(isPasswordValid('        ')).toBe(false);
+    expect(isPasswordValid('littleLittle10')).toBe(false);
+  });
+  
+  it('First Name Validator should be working', () => {
+    expect(isFirstNameValid('andy ')).toBe(true);
+  });
+  
+  it('Last Name Validator should be working', () => {
+    expect(isLastNameValid('khong')).toBe(true);
+  });
+  
+})
